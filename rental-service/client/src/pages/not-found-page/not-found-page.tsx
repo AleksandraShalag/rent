43f06db036e97b2506c962797f398cadf605deff
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-05-03 11:19:42 +0300

    Страница not-found-page теперь работает с компонентой not-found

--- rental-service/client/src/pages/not-found-page/not-found-page.tsx
+++ rental-service/client/src/pages/not-found-page/not-found-page.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
+import { NotFound } from '../../components/not-found/not-found';
 
 function NotFoundPage() {
   return (
-    <div className="not-found">
-      <h1>404</h1>
-      <h3>Page Not Found</h3>
-      <p>The requested page does not exist.</p>
-    </div>
+    <NotFound/>
   );
 }
 
