d3944d7e5fa104562b9ebce8533369cbef4a0544
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-05-03 10:41:30 +0300

    Удалены лишние компоненты. Компоненты favorites-page, login=page, not-found-page, offer-page размещены в соответствующих папках

---
+++ rental-service/client/src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h3>Page Not Found</h3>
+      <p>The requested page does not exist.</p>
+    </div>
+  );
+}
+
+export { NotFoundPage };
