57e16cbe0bde50209cdbba17ae396cf96ca0c002
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-05-16 19:31:34 +0300

    index.js дополнить настройки сервера, чтобы он мог раздавать статические изображения

--- rental-service/server/index.js
+++ rental-service/server/index.js
@@ -3,9 +3,15 @@ import * as dotenv from 'dotenv';
 import sequelize from './config/database.js';
 import cors from 'cors';
 import { router } from './routes/index.js';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
+import {fileURLToPath} from 'url';
+import path from 'path';
 
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,8 +19,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
+app.use('/', router);
 
-app.use('/api', router);
+app.use(errorMiddleware);
 
 const start = async () => {
     try{
