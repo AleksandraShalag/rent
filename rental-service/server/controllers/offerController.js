4357db41f41e2646454fb48dd3b66f88d65b1cd2
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-05-16 19:19:07 +0300

    offerController.js создадать функцию createOffer

--- rental-service/server/controllers/offerController.js
+++ rental-service/server/controllers/offerController.js
@@ -1,10 +1,98 @@
 import { Offer } from "../models/offer.js";
+import { adaptOfferToClient } from "../adapters/offerAdapter.js";
+import ApiError from "../error/ApiError.js";
+import { User } from '../models/user.js';
+import { adaptFullOfferToClient } from '../adapters/offerAdapter.js';
+
+
 async function getAllOffers(req,res,next){
     try {
         const offers = await Offer.findAll();
-        res.json(offers);
-      } catch (err) {
-        res.status(500).json({ error: 'Не удалось получить предложения' });
+        const adaptedOffers = offers.map(adaptOfferToClient);
+        res.status(200).json(adaptedOffers);
+      } catch (error) {
+        next(ApiError.internal('Не удалось получить список предложений'));
       }
 }
+
+export async function createOffer(req, res, next) {
+ try {
+   const {
+     title, description, publishDate, city,
+     isPremium, isFavorite, rating, type, rooms, guests, price,
+     features, commentsCount, latitude, longitude, userId
+   } = req.body;
+
+
+   if (!req.files?.previewImage || req.files.previewImage.length === 0) {
+     return next(ApiError.badRequest('Превью изображение обязательно для загрузки'));
+   }
+
+
+   const previewImagePath = `/static/${req.files.previewImage[0].filename}`;
+
+
+   let processedPhotos = [];
+   if (req.files?.photos) {
+     processedPhotos = req.files.photos.map(file => `/static/${file.filename}`);
+   }
+
+
+   let parsedFeatures = [];
+   if (features) {
+     try {
+       parsedFeatures = typeof features === 'string' ? JSON.parse(features) : features;
+     } catch {
+       parsedFeatures = features.split(',');
+     }
+   }
+
+
+   const offer = await Offer.create({
+     title,
+     description,
+     publishDate,
+     city,
+     previewImage: previewImagePath,
+     photos: processedPhotos,
+     isPremium,
+     isFavorite,
+     rating,
+     type,
+     rooms,
+     guests,
+     price,
+     features: parsedFeatures,
+     commentsCount,
+     latitude,
+     longitude,
+     authorId: userId
+   });
+
+
+   return res.status(201).json(offer);
+ } catch (error) {
+   next(ApiError.internal('Не удалось добавить предложение: ' + error.message));
+ }
+}
+
+
+export async function getFullOffer(req, res, next) {
+  try {
+    const { id } = req.params;
+    const offer = await Offer.findByPk(id, {
+      include: { model: User, as: 'author' }
+    });
+
+    if (!offer) {
+      return next(ApiError.badRequest('Предложение не найдено'));
+    }
+
+    const adaptedOffer = adaptFullOfferToClient(offer, offer.author);
+    res.json(adaptedOffer);
+  } catch (error) {
+    next(ApiError.internal('Ошибка при получении предложения'));
+  }
+}
+
 export {getAllOffers};
