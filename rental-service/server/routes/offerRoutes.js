9f3b3518657314930c63d3296ae7a54a45ae42a7
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-05-22 11:53:05 +0300

    Создать маршруты для получения офферов и для изменения статуса "Избранное"

--- rental-service/server/routes/offerRoutes.js
+++ rental-service/server/routes/offerRoutes.js
@@ -1,18 +1,26 @@
 import { Router } from "express";
 import upload from "../middleware/upload.js";
-import { createOffer, getAllOffers } from "../controllers/offerController.js";
+import { createOffer, getAllOffers, getFavoriteOffers, toggleFavorite } from "../controllers/offerController.js";
 import { getFullOffer } from '../controllers/offerController.js';
+import { authenticateToken } from "../middleware/authMiddleware.js";
 
-// const router = new Router();
-// router.get('/offers', getAllOffers);
 
 const router = new Router();
+
+
 router.get('/offers/:id', getFullOffer); // Новый маршрут
 
 router.post('/offers', upload.fields([
     {name: 'previewImage', maxCount: 1},
     {name: 'photos', maxCount: 6}
 ]),createOffer);
+
+
+router.get('/offers', getAllOffers);
+
+router.get('/favorite', getFavoriteOffers);
+
+router.post('/favorite/:offerId/:status', authenticateToken, toggleFavorite);
 
 
 export default router;
