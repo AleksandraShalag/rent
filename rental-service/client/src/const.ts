d355e82e529fdf02592918f6fc067c18379735f7
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-05-31 20:18:48 +0300

    6 лаба: добавили поинтеры для карты

--- rental-service/client/src/const.ts
+++ rental-service/client/src/const.ts
@@ -14,3 +14,6 @@ export const AuthorizationStatus = {
   NoAuth: 'NO_AUTH',
   Unknown: 'UNKNOWN',
 } as const;
+
+export const URL_MARKER_DEFAULT = '/img/pin.svg';
+export const URL_MARKER_CURRENT = '/img/pin-active.svg';
